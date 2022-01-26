a8e9ad66d744ec49d30cc6c08a8ea6baac9d78ee
Tin Benjamin Matuka <tin@tipka-tipka.hr>
2022-01-27 00:40:44 +0100

    Fixed session dir permissions in php-apache and updated xdebug setup

--- php-apache/entrypoint.sh
+++ php-apache/entrypoint.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Set host for xdebug on linux
-if [ -r /etc/docker_host ] && [ "$(cat /etc/docker_host)" == 'linux' ]
-then
-    gatewayIp="$(ip route | grep -E '^default' | grep -Eo '([0-9]{1,3}\.?){4}')"
-    currentHostIp="$(cat /etc/hosts | grep -E 'host.docker.internal' | grep -Eo '([0-9]{1,3}\.?){4}')"
-
-    if [ "$gatewayIp" != "$currentHostIp" ]
-    then
-        # delete line if it exists
-        if [ -n "$(grep -Eo 'host\.docker\.internal' /etc/hosts)" ]
-        then
-            sudo ex -s -c "g/host\.docker\.internal/d | wq" /etc/hosts &>/dev/null
-        fi
-
-        # add line
-        echo "${gatewayIp} host.docker.internal" | sudo tee --append /etc/hosts &>/dev/null
-    fi
-fi
-
 # use default or specified PHP version
 phpVersion="7.4"
 
